9a3484d90ab86824ef728a57da6b9b3dc7f13496
Cullin Capps <113457810+crc6267@users.noreply.github.com>
2024-03-13 16:26:01 -0400

    Themes are now saved a loaded to firestore (#43)
    There is a little latency when the user logs in a the theme changes. I
    could use some suggestions as to how to avoid this.

--- lib/screens/preferences_screen.dart
+++ lib/screens/preferences_screen.dart
@@ -1,12 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:peak/providers/theme_provider.dart';
+import 'package:peak/services/logger/default_logger.dart';
+import 'package:peak/services/logger/logger_config.dart';
 import 'package:peak/utils/themes.dart';
 import 'package:provider/provider.dart';
+import 'package:peak/services/database_service/firebase_db_service.dart';
 
 class PreferencesScreen extends StatelessWidget {
+  final _dbs = FirebaseDatabaseService.getInstance(DefaultLogger(
+      config: LoggerConfig(
+          logLevel: LogLevel.debug,
+          destination: LogDestination.console,
+          filePath: 'logs.txt')));
+
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> _saveThemeToPrefs(String theme) async {
+    final User? user = _auth.currentUser;
+
+    if (user != null) {
+      try {
+        final userInfo = {
+          'preferences': {'theme': theme}
+        };
+
+        final critera = {'docId': user.uid};
+
+        await _dbs.update('users', userInfo, critera);
+        print("User theme preference updated successfully.");
+      } catch (error) {
+        print("Error updating user theme preference: $error");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // Access your ThemeProvider
     var themeProvider = Provider.of<ThemeProvider>(context);
     ThemeData theme = Theme.of(context);
 
@@ -19,49 +49,55 @@ class PreferencesScreen extends StatelessWidget {
           ListTile(
             title: Text('Default Peak Theme',
                 style: TextStyle(color: theme.colorScheme.onPrimary)),
-            onTap: () {
+            onTap: () async {
               // Set the theme to Dark Theme
               themeProvider.setTheme(Themes.defaultTheme());
+              await _saveThemeToPrefs('default');
             },
           ),
           ListTile(
             title: Text('Dark Theme',
                 style: TextStyle(color: theme.colorScheme.onPrimary)),
-            onTap: () {
+            onTap: () async {
               // Set the theme to Dark Theme
               themeProvider.setTheme(Themes.darkModeTheme());
+              await _saveThemeToPrefs('dark');
             },
           ),
           ListTile(
             title: Text('Light Theme',
                 style: TextStyle(color: theme.colorScheme.onPrimary)),
-            onTap: () {
+            onTap: () async {
               // Set the theme to Light Theme
               themeProvider.setTheme(Themes.lightModeTheme());
+              await _saveThemeToPrefs('light');
             },
           ),
           ListTile(
             title: Text('Dark Green Theme',
                 style: TextStyle(color: theme.colorScheme.onPrimary)),
-            onTap: () {
+            onTap: () async {
               // Set the theme to Dark Green Theme
               themeProvider.setTheme(Themes.darkGreenTheme());
+              await _saveThemeToPrefs('green');
             },
           ),
           ListTile(
             title: Text('Maroon Theme',
                 style: TextStyle(color: theme.colorScheme.onPrimary)),
-            onTap: () {
+            onTap: () async {
               // Set the theme to Maroon Theme
               themeProvider.setTheme(Themes.maroonTheme());
+              await _saveThemeToPrefs('maroon');
             },
           ),
           ListTile(
             title: Text('Purple Theme',
                 style: TextStyle(color: theme.colorScheme.onPrimary)),
-            onTap: () {
+            onTap: () async {
               // Set the theme to Purple Theme
               themeProvider.setTheme(Themes.purpleTheme());
+              await _saveThemeToPrefs('purple');
             },
           ),
           // Add more themes as needed
